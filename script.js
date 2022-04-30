ceb5431ad13587f57d91ba43034ff0c28b7b5d9c
Victor <vvsh1978@mail.ru>
2022-04-30 19:03:50 +0500

    feat: make lesson26-hard

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 'use strict'
 
+let xhr = new XMLHttpRequest()
+xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts')
+
 const getData = (url) => {
     return fetch(url)
     .then(response => response.json())
     .catch(error => 'Ошибка: ' + error)
 }
 
-const setData = (url, obj) => {
-    return fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(obj),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch(error => 'Ошибка: ' + error)
-}
-
 getData('./db.json').then(data => {
-    setData('https://jsonplaceholder.typicode.com/posts', data)
+    console.log(data)
+    xhr.send(JSON.stringify(data))
 })
 
